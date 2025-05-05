e57a0fb73f91b5677dcb57fb2757a3f7c120283b
Mohamed-amine-Mr <mohamedaminemounir7@gmail.com>
2025-05-05 10:55:34 +0100

    variable variable

--- index.php
+++ index.php
@@ -1,23 +1,11 @@
-<?php $username = "Amine" ?>
+<?php
 
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <?php
-    "<meta charset=UTF-8>";
-    ?>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP | <?php echo $username ?>
-    </title>
-</head>
-
-<body>
-    <div>
-        welcom <?php echo $username ?></div>
-    <div><?php echo $username ?> you scored 1 points</div>
-    <?php include("./score.php") ?>
-</body>
-
-</html>
+/*
+    Variable Variable
+    Takes The Value Of A Variable And Treats That As The Name Of A Variable
+  */
+$a = "amine";
+$$a = "mohamed";
+echo $a;
+echo "<br>";
+echo  $amine; // because $a = amine , and we use this value as our variable name $$a
