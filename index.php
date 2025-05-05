06636b547d63a8b5c8c5fcf35fde67f09f86d5b8
Mohamed-amine-Mr <mohamedaminemounir7@gmail.com>
2025-05-05 11:04:10 +0100

    variable examples

--- index.php
+++ index.php
@@ -1,11 +1,27 @@
 <?php
 
 /*
-    Variable Variable
-    Takes The Value Of A Variable And Treats That As The Name Of A Variable
-  */
-$a = "amine";
-$$a = "mohamed";
+    Demonstrating Variable Variables in PHP
+    A variable variable allows you to use the value of a variable as the name of another variable.
+*/
+
+// Assign the string "med" to the variable $a
+$a = "med";
+
+// Create a variable with the name stored in $a (i.e., $med) and assign it the value "amine"
+$$a = "amine";
+
+// Create another variable using the value of $med (i.e., $amine) and assign it the value "Moun"
+$ $$a = "Moun";
+
+// Output the value of $a, which is "med"
 echo $a;
 echo "<br>";
-echo  $amine; // because $a = amine , and we use this value as our variable name $$a
+
+// Output the value of $med, which is "amine"
+// This works because $$a is equivalent to $med
+echo $amine;
+echo "<br>";
+
+// Output a string concatenated with the value of $med (which is "amine")
+echo "last name is ", $amine;
