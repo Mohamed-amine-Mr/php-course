19539bc75d75e2af032e5b45832537d638d38cbd
Mohamed-amine-Mr <mohamedaminemounir7@gmail.com>
2025-05-06 12:04:32 +0100

    smal exersice

--- assignments.php
+++ assignments.php
@@ -30,3 +30,13 @@
 // $b = 20;
 
 // echo $a ? $b ? $a ? $b ? $a ? $a ? $a; // 10000
+
+
+// Code 1
+$a = @$b or die("variable not exist");
+
+// Code 2
+$f = @file("Not_A_File") or die("file not exist");
+
+// Code 3
+(@include("Not_A_File")) or die("file not exist");;
