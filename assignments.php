8939f800ef095858ec219afc9b66e8dcf8d3c70c
Mohamed-amine-Mr <mohamedaminemounir7@gmail.com>
2025-05-06 11:37:24 +0100

    arrays operations

--- assignments.php
+++ assignments.php
@@ -1,87 +1,32 @@
-<!-- إستخدم ما تعلمته في المتغيرات لنستطيع تغيير جملة Elzero Courses بسهولة مستقبلا
- -->
-<!-- <?php $name = "Elzero Courses" ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Elzero Courses">
-    <title>Welcome To <?php echo $name ?></title>
-</head>
-
-<body>
-    <h1><?php echo $name ?></h1>
-    <p>Here In <?php echo $name ?> We Provide Front-End And Back-End Courses</p>
-    <hr>
-    <div><?php echo $name ?> Is The What You Need.</div>
-    <footer>All Right Reserved To <?php echo $name ?></footer>
-</body>
-
-</html> -->
-
-
-
 <?php
-// $name = "elzero";
-// $$name = "Web";
+// $a = "Elzero";
+// $b = "Web";
+// $c = "School";
 
-// echo $$name;
-// echo $elzero;
-// echo "${elzero}";
-// echo "${$name}";
-// echo $elzero;
+// $a .= " " . $b;
+// $a .= " " . $c;
+// $d = $a;
+// Method One
+// $d = " $a $b $c ";
 
-// $a = 200;
-// $b = &$a;
-// $a = 100;
+// $d = " {$a} {$b} {$c} ";
 
-// echo $b; // 100
-
+// $d =  $a . " " . $b . " " . $c;
 
 
+// Method Two
+// $d = ???;
+
+// Method Three
+// $d = ???;
+
+// Method Four
+// $d = ???;
+
+// echo $d; // Elzero Web School
 
 
-// Replace ? With Arithmetic Operators
-// echo 10 ? 20 ? 15 ? 3 ? 190 ? 10 ? 400; // 0
+// $a = 10;
+// $b = 20;
 
-
-
-$a = "10";
-// echo gettype(+$a);
-// echo +$a;
-// echo "<br>";
-
-// echo gettype(-$a);
-// echo - (-$a);
-// echo "<br>";
-
-
-// echo gettype((int)$a);
-// echo (int)$a;
-// echo "<br>";
-
-// echo gettype(intval($a));
-// echo (int)$a;
-// echo "<br>";
-
-echo gettype(($a + 0));
-echo $a + 0;
-echo "<br>";
-
-
-
-// echo $a;
-
-
-
-
-
-
-// For The People Who Love Searching
-// 10
-// "integer"
-// 10
-// "integer"
+// echo $a ? $b ? $a ? $b ? $a ? $a ? $a; // 10000
--- index.php
+++ index.php
@@ -1,31 +1,41 @@
 <?php
 
-  /*
+/*
     Operators
     - Used To Perform Operations On Values.
 
-    Arithmetic Operators
-    - Used To Do Arithmetical Operations &#038; Conversion
+    Array Operators
+    - Deal With Arrays
 
-    - $a [+]  $b => Addition
-    - $a [-]  $b => Subtraction
-    - $a [*]  $b => Multiplication
-    - $a [/]  $b => Division
-    - $a [%]  $b => Modulus
-    - $a [**] $b => Exponentiation
-    - +$a        => Identity
-    - -$a        => Negation
-  */;
-// i can do watch using %
-echo "<br>";
-// echo (21 % 10); // remove 1 to become 20
-echo "<br><br><br>";
-// echo (18 % 10); //remvoe  8 so wen devision by 10  
+    +     => Union
+    ==    => Equal => Same Key And Value
+    !=    => Not Equal
+    <>    => Not Equal
+    ===   => Identical => Same Key And Value, Same Order, Same Type
+    !==   => Not Identical
+  */
 
+$arr1 = [1 => "A", 2 => "B"];
+$arr2 = [3 => "C", 4 => "D"];
+$arr3 = $arr1 + $arr2;
 
-// echo gettype(+"100"); // change to integer
-echo "<br><br><br>";
-// echo gettype(-100);
-echo "<br><br><br>";
+echo '<pre>';
+print_r($arr1 + $arr2);
+print_r($arr3);
+echo '</pre>';
 
-echo -"-100"; // 100 
+$arr4 = [1 => "10", 2 => "20"];
+$arr5 = [2 => 20, 1 => 10];
+
+var_dump($arr4 == $arr5); // True 
+echo '<br>';
+var_dump($arr4 != $arr5); // False // bc its true == only check Same Key And Value 
+echo '<br>';
+var_dump($arr4 <> $arr5); // False 
+echo '<br>';
+
+$arr6 = [1 => 100, 2 => 200];
+$arr7 = [1 => 100, 2 => 200];
+
+var_dump($arr6 === $arr7); // Give Me True bc same order same type same key value
+var_dump($arr4 === $arr5);// fasle , bc not same order
